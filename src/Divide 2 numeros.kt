1033beeb01e87e539ec8b9ff548f477c1b03628d
DESKTOP-NJU7EFT\VipYo <yoel>
2024-12-02 11:44:55 +0100

    Update Divide 2 numeros

---
+++ src/Divide 2 numeros.kt
@@ -0,0 +1,26 @@
+fun main() {
+    var a : Double = 10.00000000000
+    var b : Double = 5.00000000000
+    var c : Double = a/b
+
+    println("a = "+ a + " b = "+ b + " a / b = " + c)
+
+    a = 10.00000000000
+    b = 3.000000000
+    c = a/b
+
+    println("a = "+ a + " b = "+ b + " a / b = " + c)
+
+    a = 1.0000000000
+    b = 0.0000000000
+    c = a/b
+
+    println("a = "+ a + " b = "+ b + " a / b = " + c)
+    a = 2.00000000000
+    b = 0.50000000000
+    c = a/b
+
+    println("a = "+ a + " b = "+ b + " a / b = " + c)
+
+
+}
