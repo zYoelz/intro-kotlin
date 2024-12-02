9916906353b39d4c7bd1fd0579a538c02feb0c89
DESKTOP-NJU7EFT\VipYo <yoel>
2024-12-02 12:18:32 +0100

    no message

---
+++ src/Divide y devuelve parte entera.kt
@@ -0,0 +1,16 @@
+import kotlin.math.roundToInt
+
+fun main(){
+    var a : Double = 4.5
+    var b : Double = 2.0
+    var c : Double = a/b
+    var d : Double = a%b
+    var f : Int
+    f = c.toInt()
+
+
+
+
+    println("a = $a b = $b a/b = $c resto = $d Entero = $f ")
+
+}
