6fbc6a9da4d2705cc424bdd10e6a28924604fe44
DESKTOP-NJU7EFT\VipYo <yoel>
2024-12-03 12:09:34 +0100

    Update Porcentage vistorias Fix resto

--- src/Divide 2 numeros.kt
+++ src/Divide 2 numeros.kt
@@ -1,23 +1,23 @@
 fun main() {
-    var a : Double = 10.00000000000
-    var b : Double = 5.00000000000
-    var c : Double = a/b
+    var a : Double = 10.0
+    var b : Double = 5.0
+    var c : Double= a/b
 
     println("a = "+ a + " b = "+ b + " a / b = " + c)
 
-    a = 10.00000000000
-    b = 3.000000000
+    a = 10.0
+    b = 3.0
     c = a/b
 
     println("a = "+ a + " b = "+ b + " a / b = " + c)
 
-    a = 1.0000000000
-    b = 0.0000000000
+    a = 1.0
+    b = 0.0
     c = a/b
 
     println("a = "+ a + " b = "+ b + " a / b = " + c)
-    a = 2.00000000000
-    b = 0.50000000000
+    a = 2.0
+    b = 0.5
     c = a/b
 
     println("a = "+ a + " b = "+ b + " a / b = " + c)
--- src/porcentaje de vistorias.kt
+++ src/porcentaje de vistorias.kt
@@ -5,11 +5,17 @@ fun main(){
     val pathName = "src/datos-partidos"
     val miArchivo = File(pathName)
     val contenido = miArchivo.readText()
-    // println(contenido)
+
+    // println(contenido) Imprime todo el contenido
 
     val lineas = miArchivo.readLines()
-    println(lineas[])
-    lineas.forEach {print(it + " --- ")}
+
+  println(lineas[1]) // Imprime la linea i+1 Empieza en 0
+
+    val partidosjugados : Int=lineas.size // Determina el numero de lineas que hay
+    println("Fueron $partidosjugados Partidos Jugados"  )
+
+    //lineas.forEach {print(it + " --- ")} Escribe las lineas una a uno, pudiendo añadir elementos entre cada linea
 
 
     var local: Int
