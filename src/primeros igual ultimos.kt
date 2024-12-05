ab8155907312d6899232f7c9c8b02d0a42d312a4
DESKTOP-NJU7EFT\VipYo <yoel>
2024-12-05 12:58:57 +0100

    update primeros igual ultimos

---
+++ src/primeros igual ultimos.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println(
+        "en una cadena dada vamos a comprovar si los 2 primeros caracteres son iguales a los 2 ultimos\n" +
+                "ahora escriba la cadena a comparar"
+    )
+    var cadena = readLine().toString()
+    val n = cadena.length
+    if (cadena.substring(0, 2) == cadena.substring(cadena.length - 2, cadena.length)) {
+        println("coincide")
+    } else {
+        println("no coincide")
+    }
+
+
+}
