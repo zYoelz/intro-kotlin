871f7a47a51b955b6c30c5b73d0210bd02d04048
DESKTOP-NJU7EFT\VipYo <yoel>
2024-12-07 00:27:01 +0100

    no message

--- src/EjerciciosArrays/Ejercicio1.kt
+++ src/EjerciciosArrays/Ejercicio1.kt
@@ -1,2 +1,12 @@
 package EjerciciosArrays
+object Ejercicio1 {
+    fun proceder(){
+        println("En este ejercicio creo una función que :\n" +
+                "Dado un array de números se insertara un número dado en una posición designada.\n" +
+                "Devuelve el nuevo array o nool en caso de que no sea posible\n")
+        println("Primero escribe la cadena de numeros")
+        val cadena = readLine().map {  }
+        println("la cadena es $cadena")
+    }
 
+}
