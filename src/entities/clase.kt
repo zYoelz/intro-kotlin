b54096d4d8fc24e88c5105b73039229b4774f657
DESKTOP-NJU7EFT\VipYo <yoel>
2024-12-09 23:14:56 +0100

    update package entities con la class clase

---
+++ src/entities/clase.kt
@@ -0,0 +1,15 @@
+package entities
+
+class Clase(
+    /** Atributos**/
+    var id: String,
+    var numAlumnos: Int,
+    var alumnosNombre: Array<String>
+
+) {
+
+}
+
+class Alumno(nombre: String) {
+
+}
