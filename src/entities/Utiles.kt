3181bcd91eb96a13dc3be6bc0656f6d8b7887367
DESKTOP-NJU7EFT\VipYo <yoel>
2024-12-12 23:07:50 +0100

    update

--- src/entities/Utiles.kt
+++ src/entities/Utiles.kt
@@ -1,14 +1,14 @@
 package entities
 
 import java.io.File
+
 var numerodeclases: Int = 0
 
 class Utiles {
 
 
-
     fun añadirAlumnos(alumnosPorClaseMax: Int, alumnos: ArrayList<Alumno>): ArrayList<Alumno> {
-        var indicemax : Float
+        var indicemax: Float
 
 
         if (alumnosTotales / 2 >= alumnosPorClaseMax) {
@@ -19,15 +19,29 @@ class Utiles {
 
         }
         var alumnado = alumnosarray
-        var a = numerodeclases*indicemax
-        val b = indicemax+a-1
+        var a = numerodeclases * indicemax
+        var b = indicemax + a - 1
         for (i in a.toInt()..b.toInt()) {
             val j = i
             val alumno = alumnado[j].split(" ").filter { it.isNotEmpty() }.toMutableList()
             alumnos.add(Alumno(j + 1, alumno[0], alumno[1].toInt()))
         }
+        var numAlumnosNoAsignados = alumnosTotales - (b +1).toInt()
+
+        if ((numerodeclases != 0) && numAlumnosNoAsignados > 0) {
+            println("Quedan $numAlumnosNoAsignados alumnos sin asignar a una clase")
+
+            for (i in b.toInt()+1 ..alumnosTotales - 1) {
+                val k = i
+                val alumnoSinAsignar = alumnado[k].split(" ").filter { it.isNotEmpty() }.toMutableList()
+                println("$alumnoSinAsignar ")
+
+            }
+
+        }
 
         numerodeclases += 1
+
         return alumnos
     }
 
--- src/entities/ejerciciosObjetosArrays/Ejercicio1.kt
+++ src/entities/ejerciciosObjetosArrays/Ejercicio1.kt
@@ -17,37 +17,12 @@ object Ejercicio1 {
                     "Las clases seran 2º E.S.O. A y 2ª E.S.O. B"
         )
         val alumnosPorClaseMax = 12
+
         val alumnosA: ArrayList<Alumno> = arrayListOf()
         val alumnosB: ArrayList<Alumno> = arrayListOf()
 
-    utiles.añadirAlumnos(alumnosPorClaseMax,alumnosA)
-        println(alumnosA)
-    utiles.añadirAlumnos(alumnosPorClaseMax,alumnosB)
-        println(alumnosB)
-        /*
-        alumnosA.add(Alumno(1, "Ana", 15))
-        alumnosA.add(Alumno(2, "Antonio", 16))
-        alumnosA.add(Alumno(3, "Lucia", 16))
-        alumnosA.add(Alumno(4, "Pablo", 15))
-        alumnosA.add(Alumno(5, "Juan", 15))
-        alumnosA.add(Alumno(6, "Isabel", 15))
-        alumnosA.add(Alumno(7, "Lua", 15))
-        alumnosA.add(Alumno(8, "Fernando", 15))
-        alumnosA.add(Alumno(9, "Paula", 15))
-        alumnosA.add(Alumno(10, "Oscar", 15))
-
-        alumnosB.add(Alumno(1, "Miguel", 15))
-        alumnosB.add(Alumno(2, "Lea", 15))
-        alumnosB.add(Alumno(3, "Alejandro", 15))
-        alumnosB.add(Alumno(4, "Roberto", 15))
-        alumnosB.add(Alumno(5, "Andrea", 15))
-        alumnosB.add(Alumno(6, "Adriana", 15))
-        alumnosB.add(Alumno(7, "Mar", 15))
-        alumnosB.add(Alumno(8, "Lidia", 15))
-        alumnosB.add(Alumno(9, "Susana", 15))
-        alumnosB.add(Alumno(10, "Daniel ", 15))
-        */
-
+        utiles.añadirAlumnos(alumnosPorClaseMax, alumnosA)
+        utiles.añadirAlumnos(alumnosPorClaseMax, alumnosB)
 
         val claseA = Clase(1, "3º E.S.O.  A", alumnosA)
         val claseB = Clase(2, "3º E.S.O.  B", alumnosB)
